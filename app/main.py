bf527523bbaba848f5b38ae7efccf465bea6cc99
Frog Chen <satgo1546@yeah.net>
2026-04-03 12:37:27 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 import threading
 import pydantic.version
 from pydantic import BaseModel
-print(pydantic.version.version_info())
+print(pydantic.version.version_info(), flush=True)
+class MyModel(BaseModel):
+    a: str
+threading.Thread(target=lambda: print(MyModel.model_validate_json('{"a":"ok"}'))).start()
