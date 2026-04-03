069302351ef8f90d59a9f7c12f564a4368545e5d
Frog Chen <satgo1546@yeah.net>
2026-04-03 12:20:07 +0800

    Add files

---
+++ app/main.py
@@ -0,0 +1,7 @@
+import threading
+import pydantic.version
+from pydantic import BaseModel
+print(pydantic.version.version_info())
+class MyModel(BaseModel):
+    a: str
+threading.Thread(target=lambda: print(MyModel.model_validate_json('{"a":"ok"}'))).start()
